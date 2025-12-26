1a789380e29e0a38509fd929770552323b15b6c9
Parth Kansara <kparth01@gmail.com>
2025-12-26 21:47:31 +0800

    Initial commit.

---
+++ src/main/java/org/example/JsonUtil.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class JsonUtil {
+
+
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    private JsonUtil() {
+    }
+
+    public static <T> T convertToObject(String jsonString, Class<T> clazz) {
+        try {
+            return mapper.readValue(jsonString, clazz);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while converting to Object");
+        }
+    }
+
+    public static String convertToString(Object object) {
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+           throw new RuntimeException("Error while converting to String");
+        }
+    }
+}
